cdc8c4d0209d9e837fe4c3e8866a9fed2a05ef61
enitefall <enitefall81@gmail.com>
2024-05-30 16:19:45 -0700

    all tasks and add tasks finished

---
+++ repository.py
@@ -0,0 +1,22 @@
+from database import new_session, TaskOrm
+from main import STaskAdd
+
+class TaskRepository:
+    @classmethod
+    async def add_task(cls, data: STaskAdd) -> int:
+        async with new_session() as session:
+            task_dict = data.model_dump()
+
+            task = TaskOrm(**task_dict)
+            session.add(task)
+            await session.flush()
+            await session.commit()
+            return task.id
+
+    @classmethod
+    async def find_all_tasks(cls):
+        async with new_session() as session:
+            query = session.query(TaskOrm)
+            result = await session.execute(query)
+            task_models = result.scalars().all()
+            return task_models
