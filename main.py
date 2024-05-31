397177a443bc6713b46611c44ea3428cdbe6da83
enitefall <enitefall81@gmail.com>
2024-05-30 17:25:10 -0700

    fixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from database import create_tables, drop_tables
+from  router import router as tasks_router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await drop_tables()
@@ -12,7 +13,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
+app.include_router(tasks_router)
 
 
 
